e183d3fab692ff8eb0015c997bb41d4e1e6f390f
lstertz <lucas@lucasstertz.com>
2021-12-31 16:23:24 -0500

    12: Moved test files.

--- Tests/EvaluatorTests.cs
+++ Tests/EvaluatorTests.cs
@@ -234,8 +234,10 @@ namespace Tests
             Evaluator<TCAttribute, TBAttribute, TDAttribute, TOAttribute> evaluator = new();
             evaluator.Initialize();
 
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
             Assert.Throws<ArgumentNullException>(() =>
                 evaluator.GetOnChangeOperations(typeof(TestBehaviorA), null));
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
         }
 
         [Test]
