552934c003b038cbd5bc56d3b7b8aa0f55cb8aad
lstertz <lucas@lucasstertz.com>
2022-07-08 00:07:06 -0500

    30: Initial console input behavior and contexts.

---
+++ IO/ConsoleInput.cs
@@ -0,0 +1,7 @@
+﻿namespace ContextualProgramming.IO;
+
+/// <summary>
+/// Defines the settings for reading input from the console.
+/// </summary>
+[Context]
+public class ConsoleInput { }
---
+++ IO/ConsoleReader.cs
@@ -0,0 +1,55 @@
+﻿using ContextualProgramming.Internal;
+
+namespace ContextualProgramming.IO;
+
+/// <summary>
+/// Reads console input as specified by <see cref="ConsoleInput"/> and 
+/// logs any read text to <see cref="ConsoleText"/>.
+/// </summary>
+[Behavior]
+[Dependency<ConsoleText>(Binding.Unique, Fulfillment.Existing, Text)]
+[Dependency<ConsoleInput>(Binding.Unique, Fulfillment.Existing, InputSettings)]
+public class ConsoleReader
+{
+    private const string InputSettings = "inputSettings";
+    private const string Text = "text";
+
+    /// <summary>
+    /// Sets up the reader's asynchronous console reader.
+    /// </summary>
+    public ConsoleReader(ConsoleInput inputSettings, ConsoleText text)
+    {
+    }
+
+    /// <summary>
+    /// Reads any available input from the console and logs it to the log.
+    /// </summary>
+    /// <param name="text">The record for input text where new input is logged.</param>
+    [Operation]
+    [OnUpdate]
+    public void ReadConsole(ConsoleText text)
+    {
+        while (Console.KeyAvailable)
+        {
+            var info = Console.ReadKey(true);
+            if (info.Key == ConsoleKey.Enter)
+            {
+                string? line = text.CurrentLine;
+                if (line != null)
+                    text.Lines.Add(line);
+
+                text.CurrentLine.Value = string.Empty;
+            }
+            else if (info.Key == ConsoleKey.Backspace)
+            {
+                string? line = text.CurrentLine;
+                if (line == null || line.Length == 0)
+                    continue;
+
+                text.CurrentLine.Value = line[0..^1];
+            }
+            else
+                text.CurrentLine.Value += info.KeyChar;
+        }
+    }
+}
