f285ac2fa9752d35cb062ead112ea8f2beef55df
lstertz <lucas@lucasstertz.com>
2022-07-24 23:44:05 -0500

    31: Add initial ConsoleOutput context.

---
+++ IO/ConsoleOutput.cs
@@ -0,0 +1,16 @@
+﻿namespace ContextualProgramming.IO;
+
+/// <summary>
+/// Defines the settings for and lines to display to the console.
+/// </summary>
+[Context]
+public class ConsoleOutput
+{
+    /// <summary>
+    /// The lines of text to be displayed to the console.
+    /// </summary>
+    /// <remarks>
+    /// Lines are expected to be ordered from earliest to latest, like a queue.
+    /// </remarks>
+    public ContextStateList<string> Lines { get; init; } = Array.Empty<string>();
+}
