e37a59db1ca86cc0fc0b179814f73a9868268f9a
lstertz <lucas@lucasstertz.com>
2022-07-25 00:02:58 -0500

    31: Stubbed ConsoleDisplayer functionality.

---
+++ IO/ConsoleDisplayer.cs
@@ -0,0 +1,24 @@
+﻿namespace ContextualProgramming.IO.Internal;
+
+/// <summary>
+/// Displays any text specified in <see cref="ConsoleOutput"/> in the console.
+/// </summary>
+[Dependency<ConsoleOutput>(Binding.Unique, Fulfillment.Existing, Output)]
+[Behavior]
+public class ConsoleDisplayer
+{
+    private const string Output = "output";
+
+
+    /// <summary>
+    /// Updates the displayed text on the console with the text 
+    /// specified by the <paramref name="output"/>.
+    /// </summary>
+    /// <param name="output">Defines the texts to be output to the console.</param>
+    [Operation]
+    [OnChange(Output)]
+    public void UpdateDisplay(ConsoleOutput output)
+    {
+
+    }
+}
