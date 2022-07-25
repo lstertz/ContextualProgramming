42299bf4a80c05964863ea680ab45b4a5d42d510
lstertz <lucas@lucasstertz.com>
2022-07-24 23:29:31 -0500

    30: Initial key input research.

---
+++ IO/KeyInput.cs
@@ -0,0 +1,22 @@
+﻿namespace ContextualProgramming.IO;
+
+/// <summary>
+/// Defines the details of any key input from the console and 
+/// the settings for handling that input.
+/// </summary>
+[Context]
+public class KeyInput
+{
+    // TODO :: Possibly update from the changes in KeyListener.
+
+    /// <summary>
+    /// The current key that is being pressed.
+    /// </summary>
+    public ContextState<ConsoleKeyInfo> ActiveKey { get; init; } = new ConsoleKeyInfo();
+
+    /// <summary>
+    /// The number of consecutive times the <see cref="ActiveKey"/> has been 
+    /// read as an input.
+    /// </summary>
+    public ContextState<int> InputCount { get; init; } = 0;
+}
